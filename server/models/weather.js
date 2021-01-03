eb7beccf02db59d5a4913150ce7ff5b7c2c8e8a5
Konrad Kurzynowski <kurzynowskikonrad@gmail.com>
2021-01-03 00:00:24 -0600

    configured project to work with env file

--- server/models/weather.js
+++ server/models/weather.js
@@ -1,13 +1,12 @@
 const request = require('request-promise');
 
-const API_KEY = '4da5a58e2ba45dee854dbe39c433e721';
-const API_KEY_ENV = process.env.REACT_APP_OWM_API;
+require('dotenv').config();
 
 // use the request method to retrieve data from open weather map api
 class Weather {
     static retrieveByCity (city, callback) {
         request({
-            uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,
+            uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.WEATHER_API_KEY}&units=imperial`,
             json: true
         }).then(function (res) {
             callback(res);
